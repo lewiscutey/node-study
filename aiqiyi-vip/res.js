97c872adfb8fa7022e12d7e605a113f04fb8330f
吕新 <949124363@qq.com>
2018-11-28 21:19:27 +0800

    thinkjs

--- aiqiyi-vip/res.js
+++ aiqiyi-vip/res.js
@@ -1,18 +1,48 @@
-var http = require('http');
-var fs = require('fs');
+var async = require('async');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+var url = require('url');
 
-http.createServer(function (request, response) {
-  fs.open('./data.json', 'r', (err, fd) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        console.error('文件不存在');
-        return;
-      }
-  
-      throw err;
+var cnodeUrl = 'https://cnodejs.org/';
+var topic =[];
+
+superagent.get(cnodeUrl)
+  .end(function(err, res){
+    if(err) {
+      return console.error(err);
     }
-  
-    response.end(fd);
+    var topicUrls = [];
+    var $ = cheerio.load(res.text);
+    $('#topic_list .topic_title').each(function (idx, element) {
+      var $element = $(element);
+      var href = url.resolve(cnodeUrl, $element.attr('href'));
+      topicUrls.push(href);
+    });  
+    
+    async.mapLimit(topicUrls, 5, function (url, callback) {
+      fetchUrl(url, callback);
+    }, function (err, result) {
+      console.log('final:');
+      console.log(result);
+      var $ = cheerio.load(topicHtml);
+      topic.push({
+        title: $('.topic_full_title').text().trim(),
+        href: topicUrl,
+        comment1: $('.reply_content').eq(0).text().trim(),
+      });
+    });
+    console.log(topic);
   });
-  
-}).listen(9996);
+
+
+var concurrencyCount = 0;
+var fetchUrl = function (url, callback) {
+  var delay = parseInt((Math.random() * 10000000) % 2000, 10);
+  concurrencyCount++;
+  console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒');
+  setTimeout(function () {
+    concurrencyCount--;
+    callback(null, url + ' html content');
+  }, delay);
+};
+
