82f8287effa52c34387620b6d031b96d8c4dfabc
lewiscutey <949124363@qq.com>
2018-12-28 22:38:17 +0800

    update app7

--- node-abc/app7.js
+++ node-abc/app7.js
@@ -1,5 +1,6 @@
-/* const readline = require('readline');
+const readline = require('readline');
 
+/* 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -25,4 +26,19 @@ rl.on('line', (line) => {
   console.log(`文件的单行内容：${line}`);
 }); */
 
+// 先来实现一个可交互命令行
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'search>>> '
+})
 
+rl.prompt()
+
+rl.on('line', (line) => {
+  console.log(line)
+  rl.prompt()
+}).on('close', () => {
+  console.log('再见!')
+  process.exit(0)
+})
