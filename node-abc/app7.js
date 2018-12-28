1ceb96dd9786b6bae638386e7daa571b99bfaef8
lewiscutey <949124363@qq.com>
2018-12-28 22:32:17 +0800

    add app7

---
+++ node-abc/app7.js
@@ -0,0 +1,28 @@
+/* const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('你叫什么名字？', (answer) => {
+  // 对答案进行处理
+  console.log(`你好：${answer}`);
+
+  rl.close();
+}); */
+
+
+/* const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('log.txt'),
+  crlfDelay: Infinity
+});
+
+rl.on('line', (line) => {
+  console.log(`文件的单行内容：${line}`);
+}); */
+
+
